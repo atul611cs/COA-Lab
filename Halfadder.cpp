c0ce71c07bbbb7993506e529c8a2ade9d3dd9213
Atul Kumar Singh <107607785+atul611cs@users.noreply.github.com>
2022-10-10 15:51:06 +0530

    Create Halfadder.cpp

---
+++ Halfadder.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,sum,carry;
+    cout<<"enter the values:";
+    cin>>a;
+    cin>>b;
+    sum=a or b;
+    carry=a and b;
+    cout<<sum<<endl;
+    cout<<carry;
+    return 0;
+}
